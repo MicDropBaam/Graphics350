d8a46de766400785fa7c379cf64c33a8c3eed779
MJ1251 <11308571+MicDropBaam@users.noreply.github.com>
2025-05-01 20:10:49 -0700

    Update Shader.h

--- Shader.h
+++ Shader.h
@@ -1,16 +1,3 @@
-/* Start Header -------------------------------------------------------
-Copyright (C) 2019 DigiPen Institute of Technology.
-Reproduction or disclosure of this file or its contents without the prior written
-consent of DigiPen Institute of Technology is prohibited.
-File Name: <Shader.h>
-Purpose: <To provide shader informations>
-Language: <c++>
-Platform: <Visual studio, window>
-Project: <minji.kim, class CS350, Assignment 3, minji.kim_CS350_3>
-Author: <Seraphina Kim, minji.kim, minji.kim>
-Creation date: <March. 15th. 2019>
-End Header --------------------------------------------------------*/
-
 #pragma once
 
 #include "glad/glad.h"
